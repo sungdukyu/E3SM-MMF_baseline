c33731ab2bf6dde02a5f3b13d39153f3224f24d9
Ritwik Gupta <ritwikg2004@live.com>
2023-06-01 01:04:28 +0000

    Update batch size search and launch scripts

--- HPO/baseline_v1/hpo_bair.py
+++ HPO/baseline_v1/hpo_bair.py
@@ -62,126 +62,137 @@ def set_environment(num_gpus_per_node=4, oracle_port="8000"):
     print("<< set_environment END >>")
 
 
-def build_model(hp, in_shape, out_shape):
-    """
-    Create a ResNet-style 1D CNN. The data is of shape (batch, lev, vars)
-    where lev is treated as the spatial dimension. The architecture
-    consists of residual blocks with each two conv layers.
-    """
-    # Define output shapes
-    output_length_lin = 2
-    output_length_relu = out_shape[-1] - 2
+class CNNHyperModel(kt.HyperModel):
+    
+    def build_model(self, hp):
+        """
+        Create a ResNet-style 1D CNN. The data is of shape (batch, lev, vars)
+        where lev is treated as the spatial dimension. The architecture
+        consists of residual blocks with each two conv layers.
+        """
+        # Define output shapes
+        in_shape = (60, 6)
+        out_shape = (60, 10)
+        output_length_lin = 2
+        output_length_relu = out_shape[-1] - 2
 
-    hp_depth = hp.Int("depth", 2, 50, default=2)
-    hp_channel_width = hp.Int("channel_width", 32, 512, default=32)
-    hp_kernel_width = hp.Choice("kernel_width", [3, 5, 7, 9], default=3)
-    hp_activation = hp.Choice(
-        "activation", ["gelu", "elu", "relu", "swish"], default="gelu"
-    )
-    hp_pre_out_activation = hp.Choice(
-        "pre_out_activation", ["gelu", "elu", "relu", "swish"], default="elu"
-    )
-    hp_norm = hp.Boolean("norm", default=False)
-    hp_dropout = hp.Float("dropout", 0.0, 0.5, default=0.0)
-    hp_optimizer = hp.Choice("optimizer", ["SGD", "Adam"], default="Adam")
-
-    channel_dims = [hp_channel_width] * hp_depth
-    kernels = [hp_kernel_width] * hp_depth
-
-    # Initialize special layers
-    norm_layer = get_normalization_layer(hp_norm)
-    if len(channel_dims) != len(kernels):
-        print(
-            f"[WARNING] Length of channel_dims and kernels does not match. Using 1st argument in kernels, {kernels[0]}, for every layer"
+        hp_depth = hp.Int("depth", 2, 50, default=2)
+        hp_channel_width = hp.Int("channel_width", 32, 512, default=32)
+        hp_kernel_width = hp.Choice("kernel_width", [3, 5, 7, 9], default=3)
+        hp_activation = hp.Choice(
+            "activation", ["gelu", "elu", "relu", "swish"], default="gelu"
         )
-        kernels = [kernels[0]] * len(channel_dims)
-
-    # Initialize model architecture
-    input_layer = keras.Input(shape=in_shape)
-    x = input_layer  # Set aside input layer
-    previous_block_activation = x  # Set aside residual
-    for filters, kernel_size in zip(channel_dims, kernels):
-        # First conv layer in block
-        # 'same' applies zero padding.
-        x = Conv1D(filters=filters, kernel_size=kernel_size, padding="same")(x)
-        # todo: add se_block
-        if norm_layer:
-            x = norm_layer(x)
-        x = keras.layers.Activation(hp_activation)(x)
-        x = keras.layers.Dropout(hp_dropout)(x)
-
-        # Second convolution layer
-        x = Conv1D(filters=filters, kernel_size=kernel_size, padding="same")(x)
-        if norm_layer:
-            x = norm_layer(x)
-        x = keras.layers.Activation(hp_activation)(x)
-        x = keras.layers.Dropout(hp_dropout)(x)
-
-        # Project residual
-        residual = Conv1D(filters=filters, kernel_size=1, strides=1, padding="same")(
-            previous_block_activation
+        hp_pre_out_activation = hp.Choice(
+            "pre_out_activation", ["gelu", "elu", "relu", "swish"], default="elu"
         )
-        x = keras.layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
+        hp_norm = hp.Boolean("norm", default=False)
+        hp_dropout = hp.Float("dropout", 0.0, 0.5, default=0.0)
+        hp_optimizer = hp.Choice("optimizer", ["SGD", "Adam"], default="Adam")
 
-    # Output layers.
-    # x = keras.layers.Dense(filters[-1], activation='gelu')(x) # Add another last layer.
-    x = Conv1D(
-        out_shape[-1], kernel_size=1, activation=hp_pre_out_activation, padding="same"
-    )(x)
-    # Assume that vertically resolved variables follow no particular range.
-    output_lin = keras.layers.Dense(output_length_lin, activation="linear")(x)
-    # Assume that all globally resolved variables are positive.
-    output_relu = keras.layers.Dense(output_length_relu, activation="relu")(x)
-    output_layer = keras.layers.Concatenate()([output_lin, output_relu])
+        channel_dims = [hp_channel_width] * hp_depth
+        kernels = [hp_kernel_width] * hp_depth
 
-    model = keras.Model(input_layer, output_layer, name="cnn")
+        # Initialize special layers
+        norm_layer = self.get_normalization_layer(hp_norm)
+        if len(channel_dims) != len(kernels):
+            print(
+                f"[WARNING] Length of channel_dims and kernels does not match. Using 1st argument in kernels, {kernels[0]}, for every layer"
+            )
+            kernels = [kernels[0]] * len(channel_dims)
 
-    # Optimizer
-    # Set up cyclic learning rate
-    INIT_LR = 1e-4
-    MAX_LR = 1e-3
-    steps_per_epoch = 10091520 // hp_depth
-    clr = tfa.optimizers.CyclicalLearningRate(
-        initial_learning_rate=INIT_LR,
-        maximal_learning_rate=MAX_LR,
-        scale_fn=lambda x: 1 / (2.0 ** (x - 1)),
-        step_size=2 * steps_per_epoch,
-        scale_mode="cycle",
-    )
+        # Initialize model architecture
+        input_layer = keras.Input(shape=in_shape)
+        x = input_layer  # Set aside input layer
+        previous_block_activation = x  # Set aside residual
+        for filters, kernel_size in zip(channel_dims, kernels):
+            # First conv layer in block
+            # 'same' applies zero padding.
+            x = Conv1D(filters=filters, kernel_size=kernel_size, padding="same")(x)
+            # todo: add se_block
+            if norm_layer:
+                x = norm_layer(x)
+            x = keras.layers.Activation(hp_activation)(x)
+            x = keras.layers.Dropout(hp_dropout)(x)
 
-    # Set up optimizer
-    if hp_optimizer == "Adam":
-        my_optimizer = keras.optimizers.Adam(learning_rate=clr)
-    elif hp_optimizer == "SGD":
-        my_optimizer = keras.optimizers.SGD(learning_rate=clr)
+            # Second convolution layer
+            x = Conv1D(filters=filters, kernel_size=kernel_size, padding="same")(x)
+            if norm_layer:
+                x = norm_layer(x)
+            x = keras.layers.Activation(hp_activation)(x)
+            x = keras.layers.Dropout(hp_dropout)(x)
 
-    # compile
-    model.compile(
-        optimizer=my_optimizer,
-        loss="mse",
-        metrics=["mse", "mae", "accuracy"],
-    )
+            # Project residual
+            residual = Conv1D(filters=filters, kernel_size=1, strides=1, padding="same")(
+                previous_block_activation
+            )
+            x = keras.layers.add([x, residual])  # Add back residual
+            previous_block_activation = x  # Set aside next residual
 
-    print(model.summary())
+        # Output layers.
+        # x = keras.layers.Dense(filters[-1], activation='gelu')(x) # Add another last layer.
+        x = Conv1D(
+            out_shape[-1], kernel_size=1, activation=hp_pre_out_activation, padding="same"
+        )(x)
+        # Assume that vertically resolved variables follow no particular range.
+        output_lin = keras.layers.Dense(output_length_lin, activation="linear")(x)
+        # Assume that all globally resolved variables are positive.
+        output_relu = keras.layers.Dense(output_length_relu, activation="relu")(x)
+        output_layer = keras.layers.Concatenate()([output_lin, output_relu])
 
-    return model
+        model = keras.Model(input_layer, output_layer, name="cnn")
+
+        # Optimizer
+        # Set up cyclic learning rate
+        INIT_LR = 1e-4
+        MAX_LR = 1e-3
+        steps_per_epoch = 10091520 // hp_depth
+        clr = tfa.optimizers.CyclicalLearningRate(
+            initial_learning_rate=INIT_LR,
+            maximal_learning_rate=MAX_LR,
+            scale_fn=lambda x: 1 / (2.0 ** (x - 1)),
+            step_size=2 * steps_per_epoch,
+            scale_mode="cycle",
+        )
+
+        # Set up optimizer
+        if hp_optimizer == "Adam":
+            my_optimizer = keras.optimizers.Adam(learning_rate=clr)
+        elif hp_optimizer == "SGD":
+            my_optimizer = keras.optimizers.SGD(learning_rate=clr)
+
+        # compile
+        model.compile(
+            optimizer=my_optimizer,
+            loss="mse",
+            metrics=["mse", "mae", "accuracy"],
+        )
+
+        print(model.summary())
+
+        return model
 
 
-def get_normalization_layer(norm=None, axis=[1, 2]):
-    """
-    Return normalization layer given string
-    Args:
-        norm string
-        axis indices for layer normalization. todo: don't hard-code
-    """
-    if norm == "layer_norm":
-        norm_layer = tf.keras.layers.LayerNormalization(axis=axis)
-    elif norm == "batch_norm":
-        norm_layer = tf.keras.layers.BatchNormalization()
-    else:
-        norm_layer = None
-    return norm_layer
+    def fit(self, hp, model, *args, **kwargs):
+        return model.fit(
+            *args,
+            batch_size=hp.Choice("batch_size", [16, 32, 64, 128, 256, 512, 1024, 2048, 4096]),
+            **kwargs,
+        )
+
+    def get_normalization_layer(self, norm=None, axis=[1, 2]):
+        """
+        Return normalization layer given string
+        Args:
+            norm string
+            axis indices for layer normalization. todo: don't hard-code
+        """
+        if norm == "layer_norm":
+            norm_layer = tf.keras.layers.LayerNormalization(axis=axis)
+        elif norm == "batch_norm":
+            norm_layer = tf.keras.layers.BatchNormalization()
+        else:
+            norm_layer = None
+        return norm_layer
 
 
 def main():
@@ -203,7 +214,7 @@ def main():
             val_target = np.load(f)
             # val_target.shape returns (1441920, 10)
     tuner = kt.Hyperband(
-        hypermodel=build_model,
+        hypermodel=CNNHyperModel(),
         objective="val_loss",
         max_trials=10,
         executions_per_trial=1,
@@ -211,27 +222,6 @@ def main():
         directory="results",
         project_name="bair",
     )
-
-    # def make_model_id(*identifiers):
-    #     return "_".join(list(identifiers))
-    # model_id = make_model_id(os.environ["KERASTUNER_TUNER_ID"])
-
-    filepath_checkpoint = "results/bair/best_models/"
-    filepath_csv = "logs/bair/training_csvs/"
-
-    # early_stopping = callbacks.EarlyStopping('val_loss', patience = 10)
-    # tboard_callback = callbacks.TensorBoard(log_dir = 'logs/bair/logs_tensorboard', histogram_freq = 1)
-    # checkpoint_callback = callbacks.ModelCheckpoint(filepath=filepath_checkpoint,
-    #                                                         save_weights_only=False,
-    #                                                         monitor='val_mse',
-    #                                                         mode='min',
-    #                                                         save_best_only=True)
-    # csv_callback = callbacks.CSVLogger(filepath_csv, separator=",", append=True)
-
-    # my_callbacks = [early_stopping,
-    #                 tboard_callback,
-    #                 checkpoint_callback,
-    #                 csv_callback]
 
     kwargs = {
         "epochs": 100,
@@ -242,13 +232,6 @@ def main():
             callbacks.TensorBoard(
                 log_dir="logs/bair/logs_tensorboard", histogram_freq=1
             ),
-            #   callbacks.ModelCheckpoint(filepath=filepath_checkpoint,
-            #                                         verbose = 1,
-            #                                         save_weights_only=False,
-            #                                         monitor='val_mse',
-            #                                         mode='min',
-            #                                         save_best_only=True),
-            #   callbacks.CSVLogger(filepath_csv, separator=",", append=True),
             callbacks.BackupAndRestore("results/bair_backup"),
         ],
     }
--- HPO/baseline_v1/sbatch-kerastuner.gpu-shared.baseline_v1.sh
+++ HPO/baseline_v1/sbatch-kerastuner.gpu-shared.baseline_v1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="HPO_v1"
+#SBATCH --job-name="Ritwik_HPO_v1"
 #SBATCH --output="logs/srun-kerastuner-%j.%N.out"
 #SBATCH --nodes=1
 #SBATCH --partition=GPU-shared
@@ -8,15 +8,14 @@
 #SBATCH --export=ALL
 #SBATCH --account=atm200007p
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=jerryL9@uci.edu
-#SBATCH -t 0:25:00
+#SBATCH --mail-user=ritwikgupta@berkeley.edu
+#SBATCH -t 12:00:00
 
 
 [[ -d ./logs/bair ]] || mkdir ./logs/bair
 
 # $1: python script name
 # $2: Keras Tuner "project name"
-source /opt/packages/anaconda3/etc/profile.d/conda.sh
-module load anaconda3
-conda activate bair
+source /ocean/projects/atm200007p/rgupta7/miniconda3/bin/activate e3sm
+module load cuda/11.7
 srun --mpi=pmi2 --wait=0 bash run-dynamic.gpu-shared.baseline_v1.sh $1
