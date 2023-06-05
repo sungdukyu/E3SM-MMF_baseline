e8f69043f9c33d4839b1364eb35995f243aeea1e
Jerry Lin <jlin96@br011.ib.bridges2.psc.edu>
2023-06-05 16:41:08 -0400

    introducing data_preprocessing class to streamline preprocessing pipeline, please feel free to make edits as necessary

---
+++ preprocessing_refactored/preprocessing_functions.py
@@ -0,0 +1,173 @@
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import glob, os
+import re
+import tensorflow as tf
+
+class data_preprocessing:
+    def __init__(self, 
+                 input_vars, 
+                 output_vars, 
+                 grid_info,
+                 inp_mean,
+                 inp_max,
+                 inp_min,
+                 out_scale):
+        self.latlim = [-999,999]
+        self.lonlim = [-999,999]
+        self.latlonnum = 384 # number of unique lat/lon grid points
+        self.input_vars = input_vars
+        self.output_vars = output_vars
+        self.grid_info = grid_info
+        self.inp_mean = inp_mean
+        self.inp_max = inp_max
+        self.inp_min = inp_min
+        self.out_scale = out_scale
+
+    def get_xrdata(self, file, file_vars = ""):
+        '''
+        This function reads in a file and returns an xarray dataset with the variables specified.
+        '''
+        ds = xr.open_dataset(file, engine = 'netcdf4')
+        if file_vars != "":
+            ds = ds[file_vars]
+        ds = ds.merge(self.grid_info[['lat','lon']])
+        ds = ds.where((ds['lat']>self.latlim[0])*(ds['lat']<self.latlim[1]),drop=True)
+        ds = ds.where((ds['lon']>self.lonlim[0])*(ds['lon']<self.lonlim[1]),drop=True)
+        return ds
+
+    def get_input(self, input_file):
+        '''
+        This function reads in a file and returns an xarray dataset with the input variables for the emulator.
+        '''
+        # read inputs
+        return self.get_xrdata(input_file, self.input_vars)
+
+    def get_output(self, output_file, input_file = ""):
+        '''
+        This function reads in a file and returns an xarray dataset with the output variables for the emulator.
+        '''
+        # read inputs
+        if input_file == "":
+            input_file = output_file.replace('.mlo.','.mli.')
+        ds_input = self.get_input(input_file)
+        
+        ds_output = self.get_xrdata(output_file)
+        # each timestep is 20 minutes which corresponds to 1200 seconds
+        ds_output['ptend_t'] = (ds_output['state_t'] - ds_input['state_t'])/1200 # T tendency [K/s]
+        ds_output['ptend_q0001'] = (ds_output['state_q0001'] - ds_input['state_q0001'])/1200 # Q tendency [kg/kg/s]
+        ds_output = ds_output[self.output_vars]
+        return ds_output
+
+    def load_ncdata_with_generator(self, filelist:list):
+        '''
+        This function works as a dataloader when training the emulator with raw netCDF files.
+        '''
+        def gen():
+            for file in filelist:
+                
+                # read inputs
+                ds_input = self.get_input(file)
+                # read outputs
+                ds_output = self.get_output(file)
+                
+                # normalization, scaling
+                ds_input = (ds_input - self.inp_mean)/(self.inp_max - self.inp_min)
+                ds_output = ds_output*self.out_scale
+
+                # stack
+                #ds = ds.stack({'batch':{'sample','ncol'}})
+                ds_input = ds_input.stack({'batch':{'ncol'}})
+                ds_input = ds_input.to_stacked_array("mlvar", sample_dims=["batch"], name='mli')
+                #dso = dso.stack({'batch':{'sample','ncol'}})
+                ds_output = ds_output.stack({'batch':{'ncol'}})
+                ds_output = ds_output.to_stacked_array("mlvar", sample_dims=["batch"], name='mlo')
+                
+                yield (ds_input.values, ds_output.values)
+
+        return tf.data.Dataset.from_generator(
+            gen,
+            output_types=(tf.float64, tf.float64),
+            output_shapes=((None,124),(None,128))
+        )
+    
+    def getdates(*args, data_path, stride_sample):
+        '''
+        This function takes in a list of file endings and returns a list of dates corresponding to the files.
+        '''
+        f_mli_data = []
+        for arg in args:
+            f_mli_data = f_mli_data + glob.glob(data_path + arg)
+        f_mli_data = sorted(f_mli_data)[::stride_sample]
+        dates = [re.sub("^.*mli\.", "", x) for x in f_mli_data]
+        dates = [re.sub("\.nc$", "", x) for x in dates]
+        repeat_dates = []
+        for date in dates:
+            for i in range(384):
+                repeat_dates.append(date)
+        return repeat_dates
+
+    def make_npy(self, 
+                 filelist:list, 
+                 prefix = "", 
+                 save_path = "", 
+                 stride_sample = 7,
+                 save_latlontime_dict = False):
+        '''
+        This function saves the training data as a .npy file.
+        '''
+        prefix = save_path + prefix
+        data_loader = self.load_ncdata_with_generator(filelist[::stride_sample])
+        npy_iterator = list(data_loader.as_numpy_iterator())
+        npy_input = np.concatenate([npy_iterator[x][0] for x in range(len(npy_iterator))])
+        npy_target = np.concatenate([npy_iterator[x][1] for x in range(len(npy_iterator))])
+        with open(save_path + prefix + '_input.npy', 'wb') as f:
+            np.save(f, np.float32(npy_input))
+        with open(save_path + prefix + '_target.npy', 'wb') as f:
+            np.save(f, np.float32(npy_target))
+        if save_latlontime_dict:
+            latlontime = {i: [(self.grid_info["lat"].values[i%384], self.grid_info["lon"].values[i%384]), val_dates[i]] for i in range(npy_input.shape[0])}
+            with open(save_path + 'indextolatlontime.pkl', 'wb') as f:
+                pickle.dump(latlontime, f)
+        return
+    
+    @classmethod
+    def set_latlim(cls, latlim):
+        '''
+        This function sets the latitude limits for reading in the data.
+        @Sungduk please provide clarity on why this is necessary.
+        '''
+        cls.latlim = latlim
+
+    @classmethod
+    def set_lonlim(cls, lonlim):
+        '''
+        This function sets the longitude limits for reading in the data.
+        @Sungduk please provide clarity on why this is necessary.
+        '''
+        cls.lonlim = lonlim
+
+    @staticmethod
+    def ls(data_path = ""):
+        '''
+        You can treat this as a Python wrapper for the bash command "ls".
+        '''
+        return os.popen(" ".join(["ls", data_path])).read().splitlines()
+    
+    @staticmethod
+    def concatenate_arrays(xrdata, vars):
+        '''
+        
+        '''
+        return np.concatenate([np.atleast_1d(xrdata[var].values) for var in vars])
+    
+
+
+
+  
+
+
+
+
