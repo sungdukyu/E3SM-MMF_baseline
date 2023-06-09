36b2e7aa38c8f90daf72711cca65d2d1fa9175fd
Jerry Lin <jlin96@br013.ib.bridges2.psc.edu>
2023-06-08 23:49:40 -0400

    made code more modular

--- refactored/e3sm_preprocessing.py
+++ refactored/e3sm_preprocessing.py
@@ -5,11 +5,13 @@ import pickle
 import glob, os
 import re
 import tensorflow as tf
+import netCDF4
+import h5py
 
 class e3sm_preprocessing:
     def __init__(self, 
                  input_vars, 
-                 output_vars, 
+                 target_vars, 
                  grid_info,
                  inp_mean,
                  inp_max,
@@ -19,12 +21,15 @@ class e3sm_preprocessing:
         self.lonlim = [-999,999]
         self.latlonnum = 384 # number of unique lat/lon grid points
         self.input_vars = input_vars
-        self.output_vars = output_vars
+        self.target_vars = target_vars
         self.grid_info = grid_info
         self.inp_mean = inp_mean
         self.inp_max = inp_max
         self.inp_min = inp_min
         self.out_scale = out_scale
+        self.train_filelist = None
+        self.val_filelist = None
+        self.test_filelist = None
 
     def get_xrdata(self, file, file_vars = ''):
         '''
@@ -45,58 +50,69 @@ class e3sm_preprocessing:
         # read inputs
         return self.get_xrdata(input_file, self.input_vars)
 
-    def get_output(self, output_file, input_file = ''):
+    def get_target(self, target_file, input_file = ''):
         '''
-        This function reads in a file and returns an xarray dataset with the output variables for the emulator.
+        This function reads in a file and returns an xarray dataset with the target variables for the emulator.
         '''
         # read inputs
         if input_file == '':
-            input_file = output_file.replace('.mlo.','.mli.')
+            input_file = target_file.replace('.mlo.','.mli.')
         ds_input = self.get_input(input_file)
         
-        ds_output = self.get_xrdata(output_file)
+        ds_target = self.get_xrdata(target_file)
         # each timestep is 20 minutes which corresponds to 1200 seconds
-        ds_output['ptend_t'] = (ds_output['state_t'] - ds_input['state_t'])/1200 # T tendency [K/s]
-        ds_output['ptend_q0001'] = (ds_output['state_q0001'] - ds_input['state_q0001'])/1200 # Q tendency [kg/kg/s]
-        ds_output = ds_output[self.output_vars]
-        return ds_output
+        ds_target['ptend_t'] = (ds_target['state_t'] - ds_input['state_t'])/1200 # T tendency [K/s]
+        ds_target['ptend_q0001'] = (ds_target['state_q0001'] - ds_input['state_q0001'])/1200 # Q tendency [kg/kg/s]
+        ds_target = ds_target[self.target_vars]
+        return ds_target
+    
+    def get_filelist(self, data_path, reg_exps, stride_sample):
+        '''
+        This function returns a list of the filepaths that match the regular expression.
+        '''
+        filelist = []
+        for reg_exp in reg_exps:
+            filelist = filelist + glob.glob(data_path + "*/" + reg_exp)
+        filelist = sorted(filelist)[::stride_sample]
+        return filelist
     
     def load_ncdata_with_generator(self, filelist:list):
         '''
         This function works as a dataloader when training the emulator with raw netCDF files.
         mli corresponds to input
-        mlo corresponds to output
+        mlo corresponds to target
         '''
         def gen():
             for file in filelist:
                 # read inputs
                 ds_input = self.get_input(file)
-                # read outputs
-                ds_output = self.get_output(file)
+                # read targets
+                ds_target = self.get_target(file)
                 
                 # normalization, scaling
                 ds_input = (ds_input - self.inp_mean)/(self.inp_max - self.inp_min)
-                ds_output = ds_output*self.out_scale
+                ds_target = ds_target*self.out_scale
 
                 # stack
                 #ds = ds.stack({'batch':{'sample','ncol'}})
                 ds_input = ds_input.stack({'batch':{'ncol'}})
                 ds_input = ds_input.to_stacked_array('mlvar', sample_dims=['batch'], name='mli')
                 #dso = dso.stack({'batch':{'sample','ncol'}})
-                ds_output = ds_output.stack({'batch':{'ncol'}})
-                ds_output = ds_output.to_stacked_array('mlvar', sample_dims=['batch'], name='mlo')
+                ds_target = ds_target.stack({'batch':{'ncol'}})
+                ds_target = ds_target.to_stacked_array('mlvar', sample_dims=['batch'], name='mlo')
                 
-                yield (ds_input.values, ds_output.values)
+                yield (ds_input.values, ds_target.values)
 
         return tf.data.Dataset.from_generator(
             gen,
-            output_types=(tf.float64, tf.float64),
-            output_shapes=((None,124),(None,128))
+            target_types=(tf.float64, tf.float64),
+            target_shapes=((None,124),(None,128))
         )
     
     def make_npy(self, 
-                 filelist:list, 
-                 prefix = '', 
+                 reg_exps = None, 
+                 prefix = '',
+                 data_path = '',
                  save_path = '', 
                  stride_sample = 7,
                  save_latlontime_dict = False):
@@ -104,15 +120,15 @@ class e3sm_preprocessing:
         This function saves the training data as a .npy file. Prefix should be train or val
         '''
         prefix = save_path + prefix
-        data_files = sorted(filelist)[::stride_sample]
+        data_files = sorted(self.get_filelist(data_path, reg_exps, stride_sample))[::stride_sample]
         data_loader = self.load_ncdata_with_generator(data_files)
         npy_iterator = list(data_loader.as_numpy_iterator())
         npy_input = np.concatenate([npy_iterator[x][0] for x in range(len(npy_iterator))])
-        npy_output = np.concatenate([npy_iterator[x][1] for x in range(len(npy_iterator))])
+        npy_target = np.concatenate([npy_iterator[x][1] for x in range(len(npy_iterator))])
         with open(save_path + prefix + '_input.npy', 'wb') as f:
             np.save(f, np.float32(npy_input))
-        with open(save_path + prefix + '_output.npy', 'wb') as f:
-            np.save(f, np.float32(npy_output))
+        with open(save_path + prefix + '_target.npy', 'wb') as f:
+            np.save(f, np.float32(npy_target))
         if save_latlontime_dict:
             dates = [re.sub('^.*mli\.', '', x) for x in data_files]
             dates = [re.sub('\.nc$', '', x) for x in dates]
@@ -143,26 +159,10 @@ class e3sm_preprocessing:
     def reshape_npy(self, var_arr, var_arr_dim):
         '''
         This function reshapes the a variable in numpy such that time gets its own axis (instead of being num_samples x num_levels).
-        Shape of output would be (timestep, lat/lon combo, num_levels)
+        Shape of target would be (timestep, lat/lon combo, num_levels)
         '''
         var_arr = var_arr.reshape((int(var_arr.shape[0]/self.latlonnum), self.latlonnum, var_arr_dim))
         return var_arr
-    
-    @classmethod
-    def set_latlim(cls, latlim):
-        '''
-        This function sets the latitude limits for reading in the data.
-        @Sungduk please provide clarity on why this is necessary.
-        '''
-        cls.latlim = latlim
-
-    @classmethod
-    def set_lonlim(cls, lonlim):
-        '''
-        This function sets the longitude limits for reading in the data.
-        @Sungduk please provide clarity on why this is necessary.
-        '''
-        cls.lonlim = lonlim
 
     @staticmethod
     def ls(data_path = ''):
@@ -172,6 +172,25 @@ class e3sm_preprocessing:
         return os.popen(' '.join(['ls', data_path])).read().splitlines()
     
     @staticmethod
+    def set_plot_params():
+        plt.close('all')
+        plt.rcParams.update(plt.rcParamsDefault)
+        plt.rc('font', family='sans')
+        plt.rcParams.update({'font.size': 32,
+                            'lines.linewidth': 2,
+                            'axes.labelsize': 32,
+                            'axes.titlesize': 32,
+                            'xtick.labelsize': 32,
+                            'ytick.labelsize': 32,
+                            'legend.fontsize': 32,
+                            'axes.linewidth': 2,
+                            "pgf.texsystem": "pdflatex"
+                            })
+        # %config InlineBackend.figure_format = 'retina'
+        # use the above line when working in a jupyter notebook
+        return
+    
+    @staticmethod
     def reshape_input_for_cnn(npy_input, save_path = ''):
         '''
         This function reshapes a numpy input array to be compatible with CNN training.
@@ -179,7 +198,6 @@ class e3sm_preprocessing:
         For the input there are 6 channels, each with 60 vertical levels.
         The last 4 channels correspond to scalars repeated across all 60 levels.
         '''
-
         npy_input_cnn = np.stack([
             npy_input[:, 0:60],
             npy_input[:, 60:120],
@@ -194,34 +212,34 @@ class e3sm_preprocessing:
         return npy_input_cnn
     
     @staticmethod
-    def reshape_output_for_cnn(npy_output, save_path = ''):
+    def reshape_target_for_cnn(npy_target, save_path = ''):
         '''
-        This function reshapes a numpy output array to be compatible with CNN training.
+        This function reshapes a numpy target array to be compatible with CNN training.
         Each variable becomes its own channel.
         For the input there are 6 channels, each with 60 vertical levels.
         The last 4 channels correspond to scalars repeated across all 60 levels.
         '''
-        npy_output_cnn = np.stack([
-            npy_output[:, 0:60],
-            npy_output[:, 60:120],
-            np.repeat(npy_output[:, 120][:, np.newaxis], 60, axis = 1),
-            np.repeat(npy_output[:, 121][:, np.newaxis], 60, axis = 1),
-            np.repeat(npy_output[:, 122][:, np.newaxis], 60, axis = 1),
-            np.repeat(npy_output[:, 123][:, np.newaxis], 60, axis = 1),
-            np.repeat(npy_output[:, 124][:, np.newaxis], 60, axis = 1),
-            np.repeat(npy_output[:, 125][:, np.newaxis], 60, axis = 1),
-            np.repeat(npy_output[:, 126][:, np.newaxis], 60, axis = 1),
-            np.repeat(npy_output[:, 127][:, np.newaxis], 60, axis = 1)], axis = 2)
+        npy_target_cnn = np.stack([
+            npy_target[:, 0:60],
+            npy_target[:, 60:120],
+            np.repeat(npy_target[:, 120][:, np.newaxis], 60, axis = 1),
+            np.repeat(npy_target[:, 121][:, np.newaxis], 60, axis = 1),
+            np.repeat(npy_target[:, 122][:, np.newaxis], 60, axis = 1),
+            np.repeat(npy_target[:, 123][:, np.newaxis], 60, axis = 1),
+            np.repeat(npy_target[:, 124][:, np.newaxis], 60, axis = 1),
+            np.repeat(npy_target[:, 125][:, np.newaxis], 60, axis = 1),
+            np.repeat(npy_target[:, 126][:, np.newaxis], 60, axis = 1),
+            np.repeat(npy_target[:, 127][:, np.newaxis], 60, axis = 1)], axis = 2)
         
         if save_path != '':
-            with open(save_path + 'train_output_cnn.npy', 'wb') as f:
-                np.save(f, np.float32(npy_output_cnn))
-        return npy_output_cnn
+            with open(save_path + 'train_target_cnn.npy', 'wb') as f:
+                np.save(f, np.float32(npy_target_cnn))
+        return npy_target_cnn
     
     @staticmethod
-    def reshape_output_from_cnn(npy_predict_cnn, save_path = ''):
+    def reshape_target_from_cnn(npy_predict_cnn, save_path = ''):
         '''
-        This function reshapes CNN output to (num_samples, 128) for standardized metrics
+        This function reshapes CNN target to (num_samples, 128) for standardized metrics
         '''
         npy_predict_cnn_reshaped = np.concatenate([
             npy_predict_cnn[:,:,0],
